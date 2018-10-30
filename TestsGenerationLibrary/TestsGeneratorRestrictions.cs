8c5ebddbfc54595229cdc7043d9579f4f4a9b882
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-10-30 22:40:09 +0300

    Create TestsGeneratorRestrictions class

---
+++ TestsGenerationLibrary/TestsGeneratorRestrictions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGenerationLibrary
+{
+    public sealed class TestsGeneratorRestrictions
+    {
+        private const int MaxConsumersCount = 8;
+
+        public int MaxReadingTasksCount { get; set; }
+        public int MaxProcessingTasksCount { get; set; }
+        public int MaxWritingTasksCount { get; set; }
+
+        public TestsGeneratorRestrictions(int maxReadingTasksCount, int maxProcessingTasksCount, int maxWritingTasksCount)
+        {
+            MaxReadingTasksCount = maxReadingTasksCount;
+            MaxProcessingTasksCount = maxProcessingTasksCount;
+
+            if (maxWritingTasksCount < 1)
+            {
+                MaxWritingTasksCount = MaxConsumersCount;
+            }
+            else
+            {
+                MaxWritingTasksCount = maxWritingTasksCount;
+            }
+        }
+    }
+}
