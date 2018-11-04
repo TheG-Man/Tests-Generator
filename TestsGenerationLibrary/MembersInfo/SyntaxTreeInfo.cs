73a916d31e67af2c32d11c9241efd3b25897d3f2
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-11-04 21:20:24 +0300

    Create SyntaxTreeInfo class

---
+++ TestsGenerationLibrary/MembersInfo/SyntaxTreeInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGenerationLibrary.MembersInfo
+{
+    class SyntaxTreeInfo
+    {
+        public IEnumerable<ClassInfo> Classes { get; }
+
+        public SyntaxTreeInfo(IEnumerable<ClassInfo> classes)
+        {
+            Classes = classes;
+        }
+    }
+}
