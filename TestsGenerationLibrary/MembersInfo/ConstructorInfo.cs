6f636cde32b309fa9c9503ecc6feddc8d9aeadd8
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-11-04 21:20:01 +0300

    Create ConstrucatorInfo class

---
+++ TestsGenerationLibrary/MembersInfo/ConstructorInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGenerationLibrary.MembersInfo
+{
+    class ConstructorInfo : MethodInfo
+    {
+        public IEnumerable<ParameterInfo> InterfaceParameters { get; }
+
+        public ConstructorInfo(IEnumerable<ParameterInfo> interfaceParameters, IEnumerable<ParameterInfo> parameters)
+            : base(null, null, parameters)
+        {
+            InterfaceParameters = interfaceParameters;
+        }
+    }
+}
