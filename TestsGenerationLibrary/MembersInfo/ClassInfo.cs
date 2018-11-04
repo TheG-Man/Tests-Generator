028ec511452e1cd8dee3ba81f4323a8d6762f8f3
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-11-04 21:20:12 +0300

    Create ClassInfo class

---
+++ TestsGenerationLibrary/MembersInfo/ClassInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGenerationLibrary.MembersInfo
+{
+    class ClassInfo
+    {
+        public string NamespaceName { get; }
+        public string Name { get; }
+        public ConstructorInfo Constructor { get; }
+        public IEnumerable<MethodInfo> Methods { get; }
+
+        public ClassInfo(string namespaceName, string name, ConstructorInfo constructor, IEnumerable<MethodInfo> methods)
+        {
+            NamespaceName = namespaceName;
+            Name = name;
+            Constructor = constructor;
+            Methods = methods;
+        }
+    }
+}
