a172cad3b2b43d89b15be47ca508b5999bffa1b4
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-11-04 21:16:30 +0300

    Create StringExtensions class

---
+++ TestsGenerationLibrary/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGenerationLibrary
+{
+    static class StringExtensions
+    {
+        public static string ToFieldName(this string target)
+        {
+            string fieldName = target.First().ToString().ToLower() + target.Substring(1, target.Length - 1);
+            return $"_{fieldName}";
+        }
+
+        public static bool IsInterfaceName(this string target)
+        {
+            return (target != null && target?.Length > 1) ? target[0] == 'I' && char.IsUpper(target[1]) : false;
+        }
+    }
+}
