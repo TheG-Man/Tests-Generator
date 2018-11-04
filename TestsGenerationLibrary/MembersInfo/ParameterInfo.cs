8ada4d0587a98c5ca4cbe384182d14e969810d61
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-11-04 21:19:27 +0300

    Create ParameterInfo class

---
+++ TestsGenerationLibrary/MembersInfo/ParameterInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGenerationLibrary.MembersInfo
+{
+    class ParameterInfo
+    {
+        public string TypeName { get; }
+        public string Name { get; }
+
+        public ParameterInfo(string typeName, string name)
+        {
+            TypeName = typeName;
+            Name = name;
+        }
+    }
+}
