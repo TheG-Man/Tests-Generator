eeeca3a6bc58a0bcfdddc51fbe9da6e1510e87af
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-11-04 21:19:42 +0300

    Create MethodInfo class

---
+++ TestsGenerationLibrary/MembersInfo/MethodInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGenerationLibrary.MembersInfo
+{
+    class MethodInfo
+    {
+        public string Name { get; }
+        public string ReturnTypeName { get; }
+        public IEnumerable<ParameterInfo> Parameters { get; }
+
+        public MethodInfo(string name, string returnTypeName, IEnumerable<ParameterInfo> parameters)
+        {
+            Name = name;
+            ReturnTypeName = returnTypeName;
+            Parameters = parameters;
+        }
+    }
+}
