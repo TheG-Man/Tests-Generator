b15173db684bb61120c190e9c6e51945a3916163
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-11-04 21:18:01 +0300

    Create FileConsumer class

---
+++ TestsGenerationLibrary/Consumers/FileConsumer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace TestsGenerationLibrary.Consumers
+{
+    public class FileConsumer : IConsumer
+    {
+        private readonly string _outputDirectoryPath;
+
+        public FileConsumer(string outputDirectoryPath)
+        {
+            _outputDirectoryPath = outputDirectoryPath;
+
+            if (!Directory.Exists(_outputDirectoryPath))
+            {
+                Directory.CreateDirectory(_outputDirectoryPath);
+            }
+        }
+
+        public void Consume(IReceivableSourceBlock<TaskResult> testTextsBuffer)
+        {
+            int thread = Thread.CurrentThread.ManagedThreadId;
+            TaskResult taskResult;
+            while (testTextsBuffer.TryReceive(out taskResult))
+            {                
+                File.WriteAllText($"{_outputDirectoryPath}\\{taskResult.FileName}", taskResult.FileData);
+            }
+        }
+    }
+}
