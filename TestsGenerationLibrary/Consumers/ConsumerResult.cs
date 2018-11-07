c64cf87b2f8804878c0836c977fc52693c5dc858
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-11-07 09:31:31 +0300

    Create ConsumerResult class

---
+++ TestsGenerationLibrary/Consumers/ConsumerResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGenerationLibrary.Consumers
+{
+    public class ConsumerResult
+    {
+        public bool IsCompletedSuccessfully { get; }
+        public object Result { get; }
+
+        public ConsumerResult(bool isCompletedSuccessfully, object result)
+        {
+            IsCompletedSuccessfully = isCompletedSuccessfully;
+            Result = result;
+        }
+    }
+}
