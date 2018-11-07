2cda4131b091d661163561ba136a9d770adb7d46
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-11-07 09:35:36 +0300

    Create TestClassInMemoryInfo

---
+++ TestsGenerationLibrary/TestClassInMemoryInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGenerationLibrary
+{
+    public class TestClassInMemoryInfo
+    {
+        public string TestClassName { get; }
+        public string TestClassData { get; }
+
+        public TestClassInMemoryInfo(string testClassName, string testClassData)
+        {
+            TestClassName = testClassName;
+            TestClassData = testClassData;
+        }
+    }
+}
