21611c9242ea9fefbea31937abd811c1978752f8
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-11-07 09:53:26 +0300

    Create ConsoleConsumer class

---
+++ TestTemplatesGenerator/ConsoleConsumer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestsGenerationLibrary;
+using TestsGenerationLibrary.Consumers;
+
+namespace TestTemplatesGenerator
+{
+    class ConsoleConsumer : IConsumer
+    {
+        public ConsumerResult Consume(TestClassInMemoryInfo testClassInMemoryInfo)
+        {
+            Console.WriteLine($"Test template name: {testClassInMemoryInfo.TestClassName}\n");
+            Console.Write(testClassInMemoryInfo.TestClassData);
+
+            return new ConsumerResult(true, null);
+        }
+    }
+}
