6286c4609ea24d1f41d0e66b08b397d7d88c327d
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-11-07 09:34:48 +0300

    Create FileSourceCodeProvider class

---
+++ TestsGenerationLibrary/DataProviders/FileSourceCodeProvider.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TestsGenerationLibrary.DataProviders
+{
+    public class FileSourceCodeProvider : ISourceCodeProvider
+    {
+        private readonly IEnumerable<string> _filePaths;
+        private readonly ParallelOptions _maxReadingTasksCount;
+
+        public FileSourceCodeProvider(IEnumerable<string> filePaths)
+            : this(filePaths, -1)
+        {
+        }
+
+        public FileSourceCodeProvider(IEnumerable<string> filePaths, int maxReadingTasksCount)
+        {
+            _filePaths = filePaths;
+
+            _maxReadingTasksCount = new ParallelOptions { MaxDegreeOfParallelism = maxReadingTasksCount };
+        }
+
+        public IEnumerable<string> Provide()
+        {
+            List<string> sourceCodesBuffer = new List<string>();
+            List<Exception> exceptions = new List<Exception>();
+
+            Parallel.ForEach(_filePaths, _maxReadingTasksCount, filePath => {
+                try
+                {
+                    sourceCodesBuffer.Add(File.ReadAllText(filePath));
+                }
+                catch (Exception exception)
+                {
+                    exceptions.Add(exception);
+                }
+            });
+
+            if (exceptions.Any())
+            {
+                throw new AggregateException(exceptions);
+            }
+
+            return sourceCodesBuffer;
+        }
+    }
+}
