71736f93703778b8c355f3d65abe93e3185b47a3
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-11-07 09:55:42 +0300

    Add minor fixes and GenerateTestsAndWriteToConsole method

--- TestTemplatesGenerator/Program.cs
+++ TestTemplatesGenerator/Program.cs
@@ -7,17 +7,18 @@ using System.Threading.Tasks.Dataflow;
 using System.IO;
 using TestsGenerationLibrary;
 using TestsGenerationLibrary.Consumers;
+using TestsGenerationLibrary.SourceCodeProviders;
 
 namespace TestTemplatesGenerator
 {
     class Program
     {
-        private const int MinParametersCount = 5;
+        private const int MinParametersCount = 4;
         private const string UsageInfo = 
             "Usage: <source_file_path> [<source_file_path>] <target_dir_path>"
             + "<max_count_tasks_to_read> <max_count_tasks_to_process> <max_count_tasks_to_write>";
-        private const string SuccessedMessage =
-            "Tests have been generated successfully";
+        private const string SuccessedMessage = "Tests have been generated successfully and saved to:";
+        private const string ErrorPromt = "The errors have been occurred:";
 
         static int Main(string[] args)
         {
@@ -29,35 +30,89 @@ namespace TestTemplatesGenerator
             }
 
             List<string> filePaths = new List<string>();
-            string targetDirectoryPath = args.ElementAt(args.Length - 4);
-            int maxCountTasksToRead = int.Parse(args.ElementAt(args.Length - 3));
+            string targetDirectoryPath = args.ElementAt(args.Length - 3);
             int maxCountTasksToProcess = int.Parse(args.ElementAt(args.Length - 2));
             int maxCountTasksToWrite = int.Parse(args.ElementAt(args.Length - 1));
-            var restrictions = new TestsGeneratorRestrictions(maxCountTasksToRead, maxCountTasksToProcess, maxCountTasksToWrite);
+            var restrictions = new TestsGeneratorRestrictions(maxCountTasksToProcess, maxCountTasksToWrite);
 
-            for (int i = 0; i < args.Length - 4; ++i)
+            for (int i = 0; i < args.Length - 3; ++i)
             {
                 filePaths.Add(args[i]);
             }
-            
-            var testsGenerator = new TestsGenerator(new FileConsumer(targetDirectoryPath), restrictions);
+
+            IEnumerable<string> generatedFilePaths = GetGeneratedFilesPaths(targetDirectoryPath, restrictions, filePaths).ToList();
+            WriteResults(generatedFilePaths);
+
+            GenerateTestsAndWriteToConsole(restrictions, filePaths);
+
+            Console.ReadKey();
+
+            return 0;
+        } 
+
+        private static IEnumerable<string> GetGeneratedFilesPaths(string targetDirectoryPath, TestsGeneratorRestrictions restrictions, IEnumerable<string> filePaths)
+        {
+            List<ConsumerResult> generatedFilePaths = new List<ConsumerResult>();
 
             try
             {
-                testsGenerator.Generate(filePaths);
+                var testsGenerator = new TestsGenerator(new FileConsumer(targetDirectoryPath), restrictions);
+                generatedFilePaths = testsGenerator.Generate(new FileSourceCodeProvider(filePaths)).ToList();
             }
             catch (AggregateException aggregateException)
             {
+                Console.WriteLine(ErrorPromt);
                 foreach (Exception exception in aggregateException.InnerExceptions)
                 {
                     Console.WriteLine(exception.Message);
                 }
             }
+            catch (ArgumentOutOfRangeException argOutOfRangeException)
+            {
+                Console.WriteLine(ErrorPromt);
+                Console.WriteLine(argOutOfRangeException.Message);
+            }
 
-            Console.WriteLine(SuccessedMessage);
-            Console.ReadKey();
+            return generatedFilePaths.Select(x => x.Result).Cast<string>();
+        }
 
-            return 0;
-        } 
+        private static void WriteResults(IEnumerable<string> generatedFilePaths)
+        {
+            if (generatedFilePaths.Any())
+            {
+                Console.WriteLine(SuccessedMessage);
+
+                for (int i = 0; i < generatedFilePaths.Count(); ++i)
+                {
+                    string filePath = generatedFilePaths.ElementAt(i);
+
+                    Console.WriteLine($"Test template {i}:");
+                    Console.WriteLine($"\tName: {Path.GetFileName(filePath)}");
+                    Console.WriteLine($"\tPath: {filePath}");
+                }
+            }
+        }
+
+        private static void GenerateTestsAndWriteToConsole(TestsGeneratorRestrictions restrictions, IEnumerable<string> filePaths)
+        {
+            try
+            {
+                var testsGenerator = new TestsGenerator(new ConsoleConsumer(), restrictions);
+                testsGenerator.Generate(new FileSourceCodeProvider(filePaths));
+            }
+            catch (AggregateException aggregateException)
+            {
+                Console.WriteLine(ErrorPromt);
+                foreach (Exception exception in aggregateException.InnerExceptions)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+            }
+            catch (ArgumentOutOfRangeException argOutOfRangeException)
+            {
+                Console.WriteLine(ErrorPromt);
+                Console.WriteLine(argOutOfRangeException.Message);
+            }
+        }
     }
 }
