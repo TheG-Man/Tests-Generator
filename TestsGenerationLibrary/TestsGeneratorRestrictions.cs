a4281742e5d90a088cf98bf61f839a51df8071df
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-11-07 09:44:06 +0300

    Reduce number of restrictions

--- TestsGenerationLibrary/TestsGeneratorRestrictions.cs
+++ TestsGenerationLibrary/TestsGeneratorRestrictions.cs
@@ -8,25 +8,13 @@ namespace TestsGenerationLibrary
 {
     public sealed class TestsGeneratorRestrictions
     {
-        private int _maxConsumersCount = Environment.ProcessorCount;
+        public int MaxProcessingTasksCount { get; }
+        public int MaxWritingTasksCount { get; }
 
-        public int MaxReadingTasksCount { get; set; }
-        public int MaxProcessingTasksCount { get; set; }
-        public int MaxWritingTasksCount { get; set; }
-
-        public TestsGeneratorRestrictions(int maxReadingTasksCount, int maxProcessingTasksCount, int maxWritingTasksCount)
+        public TestsGeneratorRestrictions(int maxProcessingTasksCount, int maxWritingTasksCount)
         {
-            MaxReadingTasksCount = maxReadingTasksCount;
             MaxProcessingTasksCount = maxProcessingTasksCount;
-
-            if (maxWritingTasksCount < 1 || maxWritingTasksCount > _maxConsumersCount)
-            {
-                MaxWritingTasksCount = _maxConsumersCount;
-            }
-            else
-            {
-                MaxWritingTasksCount = maxWritingTasksCount;
-            }
+            MaxWritingTasksCount = maxWritingTasksCount;
         }
     }
 }
